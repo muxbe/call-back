761d61ac4de4078eb7da18b326320f40cddf9614
muxbe <159569051+muxbe@users.noreply.github.com>
2024-06-25 14:33:54 +0200

    al

--- my.js
+++ my.js
@@ -8,3 +8,8 @@ const b = () => {
   console.log("its second funktion");
 };
 a(b);
+let cars = ["mersedec", "bmw", "audi", "opel"];
+cars.splice(0, 2, `subaru`);
+console.log(cars);
+let excar = cars.slice(1, 2);
+console.log(excar);
